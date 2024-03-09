a1a99013c36a1550d1b8d759e8152c2502e53a40
GreatRSingh <rakshitsingh421@gmail.com>
2024-03-09 15:22:12 +0530

    minor fix

--- app/main.py
+++ app/main.py
@@ -8,6 +8,6 @@ app = FastAPI()
 @app.post("/login/")
 async def login(file_name: Annotated[str, Form()], file: Annotated[str, Form()]):
     file_content=base64.b64decode(file)
-    with open("/app/storage/" + file_name,"w+") as f:
+    with open("./storage/" + file_name,"w+") as f:
         f.write(file_content.decode("utf-8"))
     return {"username": file_name}
