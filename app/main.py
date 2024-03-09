6a42c584d55441eb50f5a96d348dc45d3286037d
GreatRSingh <rakshitsingh421@gmail.com>
2024-03-09 15:13:37 +0530

    push

--- app/main.py
+++ app/main.py
@@ -1,18 +1,10 @@
-from fastapi import FastAPI
-import base64
+from typing import Annotated
+
+from fastapi import FastAPI, Form
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
 
-@app.get("/audio-dump")
-def audio_dump(file_name: str, file: str):
-    try:
-        wav_file = open("./app/storage/"+file_name, "wb")
-        decode_string = base64.b64decode(file)
-        wav_file.write(decode_string)
-        return {"message": "File saved"}
-    except Exception as e:
-        return {"error": e}
+@app.post("/login/")
+async def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
+    return {"username": username}
