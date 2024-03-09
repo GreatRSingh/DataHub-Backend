2f1bbc001b762d720d145d92606c7098fed5addc
GreatRSingh <rakshitsingh421@gmail.com>
2024-03-09 15:20:58 +0530

    file

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-
+import base64
 from fastapi import FastAPI, Form
 
 app = FastAPI()
@@ -7,4 +7,7 @@ app = FastAPI()
 
 @app.post("/login/")
 async def login(file_name: Annotated[str, Form()], file: Annotated[str, Form()]):
+    file_content=base64.b64decode(file)
+    with open("/app/storage/" + file_name,"w+") as f:
+        f.write(file_content.decode("utf-8"))
     return {"username": file_name}
